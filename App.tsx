0aa68bb1c3cfb4bda51b9e70a2fc2c8b9c9b8848
Aliaksei <kedraja@gmail.com>
2023-04-03 23:52:31 +0300

    [dev/MVP]: Implement custom aliases for imports

--- App.tsx
+++ App.tsx
@@ -1,26 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {SafeAreaView} from 'react-native';
+
+import {StartScreen} from '@screens';
 
 function App(): JSX.Element {
   return (
     <SafeAreaView>
-      <Text style={styles.sectionTitle}>ShopMate</Text>
+      <StartScreen />
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-});
 
 export default App;
